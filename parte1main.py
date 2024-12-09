e6d2af7c0d0f22d07d57573cacd35a3a952c1cc2
Tronodelre <albe6347@gmail.com>
2024-12-09 17:55:04 +0100

    Add files via upload

---
+++ parte1main.py
@@ -0,0 +1,8 @@
+#Inizializza il giocatore con nome e cognome. con creazione classe Partecipante
+class Partecipante:
+    def __init__(self, nome, cognome):
+        self.nome = nome
+        self.cognome = cognome
+
+    def __str__(self):
+        return f"{self.nome} {self.cognome}"
