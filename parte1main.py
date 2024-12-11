192bace6970588773ba664a7a19ebdeedbb424e4
Tronodelre <albe6347@gmail.com>
2024-12-11 12:53:32 +0100

    Classe partecipante

---
+++ parte1main.py
@@ -0,0 +1,12 @@
+#Inizializza il giocatore con nome e cognome. con creazione classe Partecipante
+class Partecipante:
+    def __init__(self, nome, cognome):
+        self.nome = nome
+        self.cognome = cognome
+
+    def __str__(self):
+        return f"{self.nome} {self.cognome}"
+
+    def __eq__ (self, other): #mi definisce l'uguaglianza tra oggetti
+        if not isinstance(other, Partecipante):
+            return self.nome == other.nome and self.cognome == other.cognome
