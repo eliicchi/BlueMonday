9b52b30fe769691e9dc3560e80e604e3f9d74fc4
eliicchi <nguyen.eli96@gmail.com>
2024-12-12 00:49:30 +0100

    aggiunta main, fix di bug

--- Gioco.py
+++ Gioco.py
@@ -12,9 +12,9 @@ class Gioco:
         self.assegnazioni = []
 
     # Creo il metodo inserisci per inserire i partecipanti del gioco
-    def inserisci(self, Nome, Cognome):
+    def inserisci(self, nome, cognome):
         # creo la variabile partecipante a cui assegno la classe partecipante
-        partecipante = Partecipante(Nome, Cognome)
+        partecipante = Partecipante(nome, cognome)
         # aggiungo la variabile partecipante alla lista dei giocatori
         self.giocatori.append(partecipante)
         print(f"Aggiunto: {partecipante}")
@@ -34,7 +34,7 @@ class Gioco:
                     # Ciclo for per controllare ogni posizione 
                     for i in range(len(self.giocatori)):
                         # Faccio un confronto tra i nomi delle liste se sono diversi
-                        if self.giocatori[0] == self.assegnazioni[0]:
+                        if self.giocatori[i] == self.assegnazioni[i]:
                             # Se i nomi della stessa posizione delle 2 liste sono uguali allora il ciclo for si interrompe e rifà la randomizzazione
                             diversi = False
                             break
@@ -50,54 +50,3 @@ class Gioco:
         else:
             print('Non ci sono giocatori')
     
-# Creo la variabile giocatore e le assegno la classe Gioco
-gioco = Gioco()
-
-# Creo la variabile richiesta che sarà l'input a cui chiedere l'utente una scelta fra quelle disponibili, in questo caso ho inserito solo la scelta di aggiungere un nuovo partecipante o terminare il programma
-# Con la funzione .strip tolgo eventuali spazi bianchi prima o dopo la stringa inserita. Con la funzione .lower rendo case insensitive l'input, in modo che digitando fine anche a caratteri diversi l'input venga preso
-richiesta = input("Digita '1' per aggiungere un nuovo partecipante\nDigita '2' per assegnare l\'amico segreto ai partecipanti\nDigita 'fine' per terminare: ").strip().lower()
-
-# Creo un ciclo while che continua finchè l'utente non digita fine
-while richiesta != "fine":
-# se l'input dell'utente è 1, chiedo all'utente nome e cognome del partecipante
-    if richiesta == "1":
-        # Chiedo all'utente il nome del partecipante e uso la funzione .strip
-        Nome = input("Inserisci il nome del partecipante: ").strip()
-        # Creo un ciclo while che continua se l'utente inserisce numeri al posto di lettere e richiede all'utente il nome del partecipante
-        # tramita la funzione all and .isalpha controllo che l'input dell'utente non contenga lettere, in caso contrario stampo un messaggio di errore e il ciclo si ripete
-        # Con la funzione .isspace permetto che il Nome possa essere composto da più parole
-        while not all(char.isalpha() or char.isspace() for char in Nome):
-            print("Errore: devi digitare solo lettere. Riprova.")
-            # Richiedo dunque all'utente il nome del partecipante
-            Nome = input("Inserisci il nome del partecipante: ").strip()
-        
-        # Chiedo all'utente il nome del partecipante e uso la funzione .strip
-        Cognome = input("Inserisci il cognome del partecipante: ").strip()
-        # tramita la funzione all and .isalpha controllo che l'input dell'utente non contenga lettere, in caso contrario stampo un messaggio di errore e il ciclo si ripete
-        # Con la funzione .isspace permetto che il Cognome possa essere composto da più parole
-        while not all(char.isalpha() or char.isspace() for char in Cognome):
-            print("Errore: devi digitare solo lettere. Riprova.")
-            Cognome = input("Inserisci il cognome del partecipante: ").strip()
-
-        # Se l'utente ha inserito il nome e il cognome corretti, richiamo il metodo inserisci della classe Gioco e aggiungo nome e cognome del partecipante come unico elemento all'interno della lista self.giocatori
-        gioco.inserisci(Nome, Cognome)
-    elif richiesta == '2':
-        gioco.assegna()
-
-    # Aggiungo un messaggio di errore se l'utente quando gli viene chiesta un'opzione o di terminare il programma sceglie un valore non previsto e faccio ripartire il ciclo richiedendo all'utente l'opzione che desidera scegliere
-    else:
-        print("Errore: comando non valido. Digita '1' o 'fine'.")
-    # Riscrivo l'input di richiesta per chiedere all'utente che opzione desidera scegliere (aggiungere partecipante o terminare) per far ripartire il ciclo in caso di errori dell'utente
-    richiesta = input("Digita '1' per aggiungere un nuovo partecipante\nDigita '2' per assegnare l\'amico segreto ai partecipanti\nDigita 'fine' per terminare: ").strip().lower()
-
-# Se tutto il ciclo prima creato non riscontra errori, il programma stampa la lista definitiva di tutti i giocatori scelti
-print("La lista definitiva dei giocatori è:")
-# utilizzo un ciclo for che itera su tutti gli elementi della lista giocatori, attributo dell'oggetto giocatore prima creato, e istanza della classe gioco
-# la variabile g rappresenta un singolo oggetto Partecipante della lista
-for g in gioco.giocatori:
-    print(g)
-
-print('Mentre la lista random è:')
-
-for g in gioco.assegnazioni:
-    print(g)
