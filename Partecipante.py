fd677c3d0b45577c3c7f74b8d6fad67f3c2fa6c9
Tronodelre <albe6347@gmail.com>
2024-12-12 09:49:11 +0100

    Update Partecipante.py

--- Partecipante.py
+++ Partecipante.py
@@ -1,11 +1,19 @@
 #Inizializza il giocatore con nome e cognome. con creazione classe Partecipante
 class Partecipante:
+   #È il costruttore della classe e serve per inizializzare gli attributi dell'oggetto
     def __init__(self, nome, cognome):
+        #Si mettono due parametri: nome e cognome, che vengono poi assegnati 
+        #agli attributi dell'oggetto tramite self.nome e self.cognome.
         self.nome = nome
         self.cognome = cognome
-
+  
+    #Mi permette di definire come un oggetto della classe Partecipante, mi 
+    #restituisce una stringa formattata con il nome e il cognome del partecipante
     def __str__(self):
         return f"{self.nome} {self.cognome}"
 
-    def __eq__ (self, other): #mi definisce l'uguaglianza tra oggetti
-        return self.nome == other.nome and self.cognome == other.cognome
+    #Mi definisce l'uguaglianza tra oggetti metodo == per gli oggetti della 
+    #classe Partecipante, mentre l'other è l'altro oggetto che viene confrontato con l'oggetto corrente
+    def __eq__ (self, other): 
+        if not isinstance(other, Partecipante):
+            return self.nome == other.nome and self.cognome == other.cognome
